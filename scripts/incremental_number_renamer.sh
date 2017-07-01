e963241a0c1f77b3c6f78170e86025dd3f32b2cc
NicolaFerracin <nicola.ferracin@gmail.com>
2017-07-01 19:42:05 +0200

    Fix to allow renaming without considering the extension and allowing names with spaces in them

--- scripts/incremental_number_renamer.sh
+++ scripts/incremental_number_renamer.sh
@@ -2,25 +2,29 @@
 echo -en "\n"
 echo "Welcome!"
 echo -en "\n"
-echo "This script allows you to rename all files with the given format in a given directory to incremental numbers, based on the amount of files present."
+echo "This script allows you to rename all files in a given directory to incremental numbers, based on the amount of files present."
 echo -en "\n"
 echo "Examples:"
-echo "- 10 jpg will be renamed from 01.jpg to 10.jpg"
-echo "- 100 jpg will be renamed from 001.jpg to 100.jpg"
+echo "- 10 files will be renamed from 01.xx to 10.xx"
+echo "- 100 files will be renamed from 001.xx to 100.xx"
 echo "... and so on."
 echo -en "\n"
 echo "IMPORTANT NOTE: the files will only be renamed, not copied. To avoid bad surprises, use this script only after doing a backup of your files."
 echo -en "\n"
 
 read -p "Enter the directory: " -i "$HOME/" -e DIR
-read -p "Enter the file format - such as JPG, PNG, TXT... (case sensitive): " FORMAT
 
-FILES=$(ls $DIR*.$FORMAT -1 | wc -l)
+FILES=$(ls $DIR* -1 | wc -l)
 DIGITS=${#FILES}
 FORMATTER=%0${DIGITS}d
 I=1
-for F in $DIR*.$FORMAT; do
-  echo "$(basename $F) will be renamed to" `printf $FORMATTER.$FORMAT $I`
+for F in $DIR*.*; do
+  EXTENSION=${F##*.}
+  if [ -z $EXTENSION ]; then
+      echo "$(basename "$F") will be renamed to" `printf $FORMATTER $I`
+  else
+      echo "$(basename "$F") will be renamed to" `printf $FORMATTER.$EXTENSION $I`
+  fi 
   I=$((I + 1))
 done
 
@@ -31,8 +35,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Started renaming..."
   echo -en "\n"
   I=1
-  for F in $DIR*.$FORMAT; do
-    mv "$F" `printf $DIR/$FORMATTER.$FORMAT $I` 2>/dev/null || true
+  for F in $DIR*.*; do
+    EXTENSION=${F##*.}
+    if [ -z $EXTENSION ]; then
+      mv "$F" `printf $DIR/$FORMATTER $I` 2>/dev/null || true
+    else
+      mv "$F" `printf $DIR/$FORMATTER.$EXTENSION $I` 2>/dev/null || true
+    fi 
     I=$((I + 1))
   done
   echo "Renaming completed with success!"
